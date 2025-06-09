707779b1ad7b7f32dff989c28ebf86f367d0ac5c
Chelo <carlosmarcelotorresvargas@gmail.com>
2025-06-09 10:26:25 -0400

    fix: agregamos cors

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,12 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,  // elimina todos los valores que llegan que no tengan un decorador en el dto
-      transform: true, // 
-      forbidNonWhitelisted: true,  // Entiendo que lanza un error por los atributos que llegan desde el front, que no esten el seteados en el dto o no tienen un decorador 
+      whitelist: true, // elimina todos los valores que llegan que no tengan un decorador en el dto
+      transform: true, //
+      forbidNonWhitelisted: true, // Entiendo que lanza un error por los atributos que llegan desde el front, que no esten el seteados en el dto o no tienen un decorador
     }),
   );
-
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
