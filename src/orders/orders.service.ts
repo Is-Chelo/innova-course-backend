90659d78d742d6f6991cd8a557d636458390add4
Chelo <carlosmarcelotorresvargas@gmail.com>
2025-08-08 18:54:16 -0400

    fix

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -11,64 +11,46 @@ export class OrdersService {
   ) {}
 
   async create(createOrderDto: CreateOrderDto) {
-    try {
-      const { orderItems } = createOrderDto;
-      // Validar que todos los productos del array existan
-      const productIds = createOrderDto.orderItems.map((product) => {
-        return product.productId;
-      });
+    const { orderItems } = createOrderDto;
+    // Validar que todos los productos del array existan
+    const productIds = createOrderDto.orderItems.map((product) => {
+      return product.productId;
+    });
 
-      const productIdsExist = await this.prisma.product.findMany({
-        where: {
-          id: {
-            in: productIds,
-          },
+    const productIdsExist = await this.prisma.product.findMany({
+      where: {
+        id: {
+          in: productIds,
         },
-      });
-      if (productIds.length !== productIdsExist.length) {
-        return {
-          status: false,
-          message: 'No se encontro un producto.',
-          detail: '',
-        };
-        // throw new BadRequestException('No se encontro un producto.');
-      }
-
-      // Actualizamos el stock
-      productIdsExist.map((product, index) => {
-        if (product.stock < orderItems[index].quantity) {
-          return {
-            status: false,
-            message: 'No hay stock sucifiente.',
-            detail: '',
-          };
-          // throw new BadRequestException('No hay stock sucifiente.');
-        }
-      });
-
-      productIdsExist.map(async (product, index) => {
-        const newStock = product.stock - orderItems[index].quantity;
-        await this.productService.update(product.id, {
-          stock: newStock,
-        });
-      });
-
-      // Guardamos la orden
-      const newOrder = await this.prisma.order.create({
-        data: createOrderDto,
-      });
-
-      return {
-        message: 'Orden Registrada.',
-        order: newOrder,
-      };
-    } catch (error) {
-      return {
-        status: false,
-        message: 'Hubo un error.',
-        detail: '',
-      };
+      },
+    });
+    if (productIds.length !== productIdsExist.length) {
+      throw new BadRequestException('No se encontro un producto.');
     }
+
+    // Actualizamos el stock
+    productIdsExist.map((product, index) => {
+      if (product.stock < orderItems[index].quantity) {
+        throw new BadRequestException('No hay stock sucifiente.');
+      }
+    });
+
+    productIdsExist.map(async (product, index) => {
+      const newStock = product.stock - orderItems[index].quantity;
+      await this.productService.update(product.id, {
+        stock: newStock,
+      });
+    });
+
+    // Guardamos la orden
+    const newOrder = await this.prisma.order.create({
+      data: createOrderDto,
+    });
+
+    return {
+      message: 'Orden Registrada.',
+      order: newOrder,
+    };
   }
 
   async findAll() {
